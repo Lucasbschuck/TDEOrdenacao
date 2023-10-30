53abb45e8058f7156096acc93f9931d69886f4ee
Henrique-zan <110859768+Henrique-zan@users.noreply.github.com>
2023-10-30 10:30:51 -0300

    Add files via upload

--- SelectionSort.java
+++ SelectionSort.java
@@ -2,20 +2,23 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-
 package selectionsort;
 
 import java.util.Random;
-import java.util.Scanner;
+
 
 public class SelectionSort {
+    
+    static int nr_trocas = 0;
+
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        
+     long startTime = System.nanoTime();
+        
         Random random = new Random(123456789);
-
-        System.out.print("Tamanho do vetor: ");
-        int tamanho = scanner.nextInt();
+        
+        int tamanho = 50;
 
         int[] vetor = new int[tamanho];
         for (int i = 0; i < tamanho; i++) {
@@ -33,6 +36,11 @@ public class SelectionSort {
         for (int num : vetor) {
             System.out.print(num + " ");
         }
+        long endTime = System.nanoTime();
+        long duration = (endTime - startTime) / 1000000;
+
+        System.out.println("\n\nTempo de execucao de insercao: " + duration + " milissegundos");
+        System.out.print("Numero de trocas: " + nr_trocas + "\n\n");
     }
 
     public static void ordenarSelecao(int[] array, int tamanho) {
@@ -41,12 +49,16 @@ public class SelectionSort {
             for (int j = i + 1; j < tamanho; j++) {
                 if (array[j] < array[indiceMenor]) {
                     indiceMenor = j;
+                    nr_trocas += 1;
+
                 }
             }
             int temp = array[indiceMenor];
             array[indiceMenor] = array[i];
             array[i] = temp;
-        } 
+           
+        }
+        
     }
+    
 }
-
